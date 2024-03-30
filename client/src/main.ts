5ac2903887d37cc83a30e3ff50bf2160e3dffe25
POTI <potistudio2022@gmail.com>
2024-03-31 00:00:08 +0900

    feat: Hello World

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,15 @@
 
+import { DiscordSDK } from "@discord/embedded-app-sdk";
+
 import "./style.css";
 import App from "./App.svelte";
 
+//* Initialize Discord App
+const discordSDK = new DiscordSDK (import.meta.env.VITE_DISCORD_CLIENT_ID);
+
+discordSDK.ready().then (() => console.log("Discord SDK is Ready"));
+
+//* Initialize Svelte
 const app = new App ({
 	target: document.getElementById ("app")!
 });
